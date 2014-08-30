35256706cd567b90e9bd2f19fa941b11bfc2751b
Pooja Sethi <sethi.pooja@gmail.com>
2014-08-30 00:02:40 -0700

    Created intent to launch pay scale activity from submission confirmation

--- Respeak/src/com/uw/ictd/respeak/SubmissionConfirmationActivity.java
+++ Respeak/src/com/uw/ictd/respeak/SubmissionConfirmationActivity.java
@@ -1,15 +1,29 @@
 package com.uw.ictd.respeak;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class SubmissionConfirmationActivity extends Activity {
+	
+	private Button mViewPayScaleButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_submission_confirmation);
+		
+		mViewPayScaleButton = (Button) findViewById(R.id.viewPayScaleButton);
+		
+		mViewPayScaleButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(SubmissionConfirmationActivity.this, PayScaleActivity.class);
+				startActivity(i);
+			}
+		});
 	}
 }
