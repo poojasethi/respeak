69b37c4c16e578c6ce728c629c55cce7bedca79c
Pooja Sethi <sethi.pooja@gmail.com>
2014-09-03 17:51:24 -0700

    Updated layout for account balance

--- Respeak/src/com/uw/ictd/respeak/AccountBalanceActivity.java
+++ Respeak/src/com/uw/ictd/respeak/AccountBalanceActivity.java
@@ -1,7 +1,23 @@
 package com.uw.ictd.respeak;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.os.Bundle;
 
 public class AccountBalanceActivity extends Activity {
-
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_account_balance);
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		
+		FragmentManager fm = getFragmentManager();
+		Fragment fragment = fm.findFragmentById(R.id.trainingFragmentContainer);
+		
+		if (fragment == null) {
+			fragment = new TrainingFragment();
+			fm.beginTransaction().add(R.id.trainingFragmentContainer, fragment).commit();
+		}
+	}
 }
