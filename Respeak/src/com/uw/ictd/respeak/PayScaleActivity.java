9c444679c041b583e51e7f97f096f8602f4684d4
Pooja Sethi <sethi.pooja@gmail.com>
2014-09-11 12:40:26 -0700

    Removed action bar icons from pay scale

--- Respeak/src/com/uw/ictd/respeak/PayScaleActivity.java
+++ Respeak/src/com/uw/ictd/respeak/PayScaleActivity.java
@@ -1,13 +1,26 @@
 package com.uw.ictd.respeak;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class PayScaleActivity extends MenuActivity {
+public class PayScaleActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_pay_scale);
 		getActionBar().setDisplayHomeAsUpEnabled(true);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			onBackPressed();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
 }
