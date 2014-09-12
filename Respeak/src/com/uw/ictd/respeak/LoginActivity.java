bf49cddff124efd7f33fa3011cd363df0753ea44
Pooja Sethi <sethi.pooja@gmail.com>
2014-09-11 17:05:24 -0700

    Remembers if it is the user's first time on Respeak and launches appropriate activity

--- Respeak/src/com/uw/ictd/respeak/LoginActivity.java
+++ Respeak/src/com/uw/ictd/respeak/LoginActivity.java
@@ -3,6 +3,7 @@ package com.uw.ictd.respeak;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
 import android.view.Gravity;
@@ -16,10 +17,12 @@ import android.widget.Toast;
 
 public class LoginActivity extends Activity {
 
+	public static final String PREFS = "PrefsFile";
 	private Button mLogInButton;
 	private Button mSignUpButton;
 	private EditText mPhoneNumberField;
 	private String mPhoneNumber;
+	private static boolean mFirstTimeUser;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -33,21 +36,39 @@ public class LoginActivity extends Activity {
 		// Hides keyboard until user selects edit text (phone number field)
 		this.getWindow().setSoftInputMode(
 				WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
-		
-		// Automatically detect the phone number and populates the edit text field
-		mPhoneNumber = getPhoneNumber();
+
+		// Automatically detect the phone number and populates the edit text
+		// field
+
+		// Restore preferences
+		SharedPreferences settings = getSharedPreferences(PREFS, MODE_PRIVATE);
+		mPhoneNumber = settings.getString("phoneNumber", null);
+
+		// If the phone number isn't already saved in preferences, this is a
+		// first-time user
+		if (mPhoneNumber == null) {
+			mPhoneNumber = getPhoneNumber();
+			mFirstTimeUser = true;
+		}
 		mPhoneNumberField.setText(mPhoneNumber, TextView.BufferType.EDITABLE);
 
 		mLogInButton.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-				Intent i = new Intent(LoginActivity.this, ListenActivity.class);
+				Intent i;
 				
+				// Set the appropriate activity to be launched
+				if (mFirstTimeUser) {
+					i = new Intent(LoginActivity.this, TrainingActivity.class);
+				} else {
+					i = new Intent(LoginActivity.this, ListenActivity.class);
+				}
+
 				// Allow user to set the number
 				mPhoneNumber = mPhoneNumberField.getText().toString();
 
-				// Check that the user put in a phone number
+				// Check that the user put in a valid phone number
 				if (checkPhoneNumber(mPhoneNumber)) {
 					startActivity(i);
 				}
@@ -67,7 +88,21 @@ public class LoginActivity extends Activity {
 
 	}
 
-	// TODO: check that the phone number is actually a valid number
+	@Override
+	protected void onStop() {
+		super.onStop();
+
+		// If this was a first time user, save their phone number
+		if (mFirstTimeUser) {
+			SharedPreferences settings = getSharedPreferences(PREFS, MODE_PRIVATE);
+			SharedPreferences.Editor editor = settings.edit();
+			editor.putString("phoneNumber", mPhoneNumber);
+			mFirstTimeUser = false;
+			editor.commit();
+		}
+	}
+
+	// Check that the phone number is actually a valid number
 	private boolean checkPhoneNumber(String phoneNumber) {
 		if (phoneNumber.length() < 10) {
 			Toast toast = Toast.makeText(this,
@@ -78,9 +113,11 @@ public class LoginActivity extends Activity {
 		}
 		return true;
 	}
-	
+
+	// Read phone number from phone
 	private String getPhoneNumber() {
-		TelephonyManager tMgr = (TelephonyManager) getApplicationContext().getSystemService(Context.TELEPHONY_SERVICE);
+		TelephonyManager tMgr = (TelephonyManager) getApplicationContext()
+				.getSystemService(Context.TELEPHONY_SERVICE);
 		return tMgr.getLine1Number();
 	}
 }
